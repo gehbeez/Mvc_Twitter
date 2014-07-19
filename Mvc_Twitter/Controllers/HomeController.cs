adf94633d39cb9d2d390e39dae2ad92e943358fd
gehbeez <mahmoud.elmancy@gmail.com>
2014-07-19 23:06:55 +0300

    new

--- Mvc_Twitter/Controllers/HomeController.cs
+++ Mvc_Twitter/Controllers/HomeController.cs
@@ -9,42 +9,44 @@ namespace Mvc_Twitter.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-        [HttpGet]
+
+
         public ActionResult Index()
         {
-            //TODO Please use your own keys and the secrets!
-
-            //TwitterService("consumer key", "consumer secret");
-            var service = new TwitterService("", "");
-
-            //AuthenticateWith("Access Token", "AccessTokenSecret");
-            service.AuthenticateWith("", "");
-
-            //ScreenName="screeen name not username", Count=Number of Tweets / www.Twitter.com/mcansozeri. 
-            //IEnumerable<TwitterStatus> tweets = service.ListTweetsOnUserTimeline(new ListTweetsOnUserTimelineOptions { ScreenName = txtTwitterName, Count = 5,});
-
-            var options = new SearchOptions { Q = "%23ACME", Count = 10000 };
-
-            var tweets = service.Search(options);
-
-            foreach (var tweet in tweets.Statuses)
-            {
-                Console.WriteLine("{0} says '{1}'", tweet.User.Location, tweet.Text);
-            }
-
-
-            ViewBag.Tweets = tweets.Statuses;
-
             return View();
         }
+        //
+        // GET: /Home/
+        //[HttpGet]
+        //public ActionResult Index_Code()
+        //{
+        //    //TODO Please use your own keys and the secrets!
 
-        public ActionResult Tweet()
-        {
+        //    //TwitterService("consumer key", "consumer secret");
+        //    var service = new TwitterService("", "");
 
-            return View();
-        }
+        //    //AuthenticateWith("Access Token", "AccessTokenSecret");
+        //    service.AuthenticateWith("", "");
+
+        //    //ScreenName="screeen name not username", Count=Number of Tweets / www.Twitter.com/mcansozeri. 
+        //    //IEnumerable<TwitterStatus> tweets = service.ListTweetsOnUserTimeline(new ListTweetsOnUserTimelineOptions { ScreenName = txtTwitterName, Count = 5,});
+
+        //    var options = new SearchOptions { Q = "%23ACME", Count = 10000 };
+
+        //    var tweets = service.Search(options);
+
+        //    foreach (var tweet in tweets.Statuses)
+        //    {
+        //        Console.WriteLine("{0} says '{1}'", tweet.User.Location, tweet.Text);
+        //    }
+
+
+        //    ViewBag.Tweets = tweets.Statuses;
+
+        //    return View();
+        //}
+
+
 
     }
 }
